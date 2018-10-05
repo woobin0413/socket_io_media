0b1d85a01af4e0d9221961d752a3ebd285681052
woobin <woobin0413@gmail.com>
2018-10-04 19:55:46 -0500

    socket
    .io

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000,function(){
-  console.log('Connected, 3000 port!');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//look for a index.html file in views folder
+app.use(express.static('views'));
+
+ //client connection current status
+ io.on('connection', function(socket){
+   console.log('a user connected', socket.id);
+   socket.on('disconnect', function(){
+     console.log('user disconnected', socket.id);
+   });
+ });
+
+ //chat message printed in console
+ io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
-//static files
- app.set('view engine', 'pug');
- app.set('views','./views/');
+//chat message shared by connected users
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
- app.get('/', function(req,res){
-   res.render('index');
- })
-// app.use(express.static('views'));
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
