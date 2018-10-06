874e5099a410f472d982f5377b5b56b74d7fd017
woobin <woobin0413@gmail.com>
2018-10-05 19:31:10 -0500

    update
    need to be fixed

--- index.js
+++ index.js
@@ -1,58 +1,26 @@
 var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var users = [];
-//look for a index.html file in views folder
+var app = require('express')();
+var server = require('http').createServer(app);
+// http server를 socket.io server로 upgrade한다
+var io = require('socket.io')(server);
 app.use(express.static('views'));
+// localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
 
-io.on('connection',function(socket){
-  socket.on('login',function(data){
-    console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
+// namespace /chat에 접속한다.
+var chat = io.of('/chat').on('connection', function(socket) {
+  socket.on('chat message', function(data){
+    console.log('message from client: ', data);
 
-    // socket에 클라이언트 정보를 저장한다
-    socket.name = data.name;
-    socket.userid = data.userid;
-    // 접속된 모든 클라이언트에게 메시지를 전송한다
-    io.emit('login', data.name );
+    var name = socket.name = data.name;
+    var room = socket.room = data.room;
+
+    // room에 join한다
+    socket.join(room);
+    // room에 join되어 있는 클라이언트에게 메시지를 전송한다
+    chat.to(room).emit('chat message', data.msg);
   });
-
-  socket.on('chat',function(data){
-    console.log('Message from %s: %s', socket.name, data.msg);
-
-    var msg = {
-      from: {
-        name:socket.name,
-        userid: socket.userid
-      },
-      msg: data.msg
-    };
-    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
-    socket.broadcast.emit('chat', msg);
-
-  });
-
-  socket.on('disconnect',function(){
-    io.emit('chat message', socket.name + " user disconnected");
-    socket.disconnect();
-  })
 });
 
-//  //client connection current status
-//  io.on('connection', function(socket){
-//    console.log('a user connected', socket.id);
-//    socket.on('disconnect', function(){
-//      io.emit('chat message', socket.id + " user disconnected");
-//    });
-//  });
-//
-// io.on('connection', function(socket){
-//   io.emit('chat message', socket.id + " user connected");
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', socket.id + " : "+msg);
-//   });
-// });
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+server.listen(3000, function() {
+  console.log('Socket IO server listening on port 3000');
 });
