113f91afa58ea1cbefb7358bc96cbd533653ebee
woobin <woobin0413@gmail.com>
2018-10-19 22:03:02 -0500

    .
    .

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-
-
-var express = require('express')();
+// Setup basic express server
+var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
+server.listen(3000, function() {
+  console.log('Server listening at port on 3000');
+});
 
+// Routing
 app.use(express.static('public'));
 
+io.on('connection', function(socket) {
 
-io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-});
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function() {
+      // echo globally that this client has left
+      console.log('user disconnected');
+      socket.broadcast.emit('user left');
+    });
+  });
