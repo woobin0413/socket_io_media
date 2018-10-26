e67b71e99ef8a179ef1acdbf4c2338bbeec8a4aa
woobin <woobin0413@gmail.com>
2018-10-25 19:58:57 -0500

    .
    .

---
+++ app_socket.js
@@ -0,0 +1,36 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
+var fetchVideoInfo = require('youtube-info');
+app.locals.pretty = true;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'pug');
+app.set('views','./view_socket');
+
+server.listen(3000, function() {
+  console.log('Server listening at port on 3000!!!!');
+});
+
+app.get('/',function(req,res){
+  res.render('view');
+})
+
+
+io.on('connection', function(socket) {
+  socket.broadcast.emit('user joined', socket.id);
+  fetchVideoInfo('zOeUbbJXtRQ').then(function (videoInfo) {
+    socket.broadcast.emit('iframe info', videoInfo.duration);
+  });
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect',function(){
+      socket.broadcast.emit('user left');
+  });
+});
