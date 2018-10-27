649bf7ea4d556cb7463dd202196215697e8cf498
woobin <woobin0413@gmail.com>
2018-10-26 19:36:57 -0500

    .
    .

--- view_socket/main.js
+++ view_socket/main.js
@@ -1,11 +1,32 @@
 $(function () {
-    var socket = io();
-    $('form').submit(function(){
-      socket.emit('chat message', $('#m').val());
-      $('#m').val('');
-      return false;
-    });
-    socket.on('chat message', function(msg){
-      $('#messages').append($('<p>').text(msg));
-    });
+  var socket = io();
+  $('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
   });
+  socket.on('chat message', function(msg){
+    $('#messages').append($('<p>').text(msg));
+  });
+
+
+
+});
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '360',
+    width: '640',
+    videoId: 'zOeUbbJXtRQ'
+  });
+};
+
+function onclickPause() {
+  player.pauseVideo();
+};
+
+function onclickStart() {
+  player.playVideo();
+
+};
