07a3590bfa110e1a7750d1874ae6627e9c2709ad
woobin <woobin0413@gmail.com>
2018-10-29 19:46:14 -0500

    documentation 1

--- app_socket.js
+++ app_socket.js
@@ -19,30 +19,31 @@ app.get('/',function(req,res){
   res.render('view');
 })
 
-
+//when a user joins this server
 io.on('connection', function(socket) {
   socket.broadcast.emit('user joined');
 
-
+//chat message receives from a client then sends back to other client
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 
+//video time client <-> server
   socket.on('video time', function(time){
     io.emit('video time', time);
-
   });
 
+//video start client <-> server
   socket.on('video start', function(){
     io.emit('video start');
   });
 
+//video stop client <-> server
   socket.on('video stop', function(){
     io.emit('video stop');
   });
 
-
-
+//video disconnect client <-> server
   socket.on('disconnect',function(){
       socket.broadcast.emit('user left');
   });
